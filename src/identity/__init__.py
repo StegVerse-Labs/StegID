541d0ea7b75ff04a0365b136f93eb9edd30b0cfd
StegVerse <rigelrandolph@gmail.com>
2025-12-23 02:28:35 -0600

    Update __init__.py

--- src/identity/__init__.py
+++ src/identity/__init__.py
@@ -1,1 +1,3 @@
-from .verify_entrypoint import verify_receipt_payload_bytes
+from .envelope import StegIDEnvelope, ReceiptHint, make_receipt_envelope
+from .verify_entrypoint import verify_receipt_payload_bytes, VerificationError, VerifiedReceipt
+from .keyring import KeyringStore
