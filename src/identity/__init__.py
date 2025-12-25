8e29f7cc9f0ea9d384fbceaaf68d046a16f03989
StegVerse <rigelrandolph@gmail.com>
2025-12-24 21:55:52 -0600

    Update __init__.py

--- src/identity/__init__.py
+++ src/identity/__init__.py
@@ -10,15 +10,7 @@ from .verify_entrypoint import (
     VerifiedReceipt,
 )
 
-from .entity_identity import (
-    root_key_id_from_public_pem,
-    entity_id_from_public_pem,
-    entity_id_from_root_key_id,
-)
-
 from .keyring import KeyringStore
-
-from .stegtv_adapter import StegTVContinuityAdapter
 
 __all__ = [
     "VerificationError",
@@ -27,9 +19,5 @@ __all__ = [
     "verify_chain_and_sequence",
     "verify_receipt_payload_bytes",
     "VerifiedReceipt",
-    "root_key_id_from_public_pem",
-    "entity_id_from_public_pem",
-    "entity_id_from_root_key_id",
     "KeyringStore",
-    "StegTVContinuityAdapter",
 ]
